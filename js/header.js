647e87746c3d9cea561b10c23114ac4b7160f4b9
Vendigo <119477860+Vendifo@users.noreply.github.com>
2024-03-01 05:43:45 +0900

    about и мелкие правки

--- js/header.js
+++ js/header.js
@@ -21,14 +21,12 @@ document.getElementById('toggleBtn').addEventListener('click', function() {
             toggleBtn.innerHTML = '&#10006;';
             toggleBtn.style.left = '0px';
             mainContent.style.marginLeft = '200px';
-            section.style.margin = '0px 60px';
         } else {
             header.style.left = '-300px';
             header.classList.add('closed');
             toggleBtn.innerHTML = '&#9776;';
             toggleBtn.style.left = ''; 
             mainContent.style.marginLeft = '0';
-            section.style.margin = '0px 160px';
             
         }
     }
@@ -43,3 +41,45 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 });
 
+
+// Функция для обновления стилей в зависимости от размера окна
+function updateStyles() {
+    var header = document.getElementById('header');
+    var toggleBtn = document.getElementById('toggleBtn');
+    var mainContent = document.getElementById('main');
+    var section = document.getElementById('section');
+
+    if (window.innerWidth <= 710) {
+        header.style.left = '0'; // Возвращаем header на место, если он был скрыт на компьютере
+        if (header.classList.contains('closed')) {
+            header.style.top = '0';
+            toggleBtn.innerHTML = '&#10006;';
+        } else {
+            header.style.top = '-100%';
+            toggleBtn.innerHTML = '&#9776;';
+        }
+        toggleBtn.style.left = ''; // Сбрасываем стили кнопки toggleBtn
+        mainContent.style.marginLeft = '0'; // Сбрасываем стили для mainContent
+        section.style.margin = '0px 20px'; // Уменьшаем margin для section на мобильных устройствах
+    } else {
+        header.style.top = '0'; // Возвращаем header на место, если он был скрыт на мобильном устройстве
+        if (header.classList.contains('closed')) {
+            header.style.left = '-300px';
+            toggleBtn.innerHTML = '&#9776;';
+        } else {
+            header.style.left = '0';
+            toggleBtn.innerHTML = '&#10006;';
+        }
+        section.style.margin = '0px 160px'; // Устанавливаем margin для section на компьютере
+    }
+}
+
+// Добавляем обработчик события изменения размера окна
+window.addEventListener('resize', function() {
+    updateStyles();
+});
+
+// Вызываем функцию updateStyles() при загрузке страницы
+document.addEventListener('DOMContentLoaded', function() {
+    updateStyles();
+});
